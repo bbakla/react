e83987adc997c33571cf2615bf06674e4328da85
Baris Bakla <baris.bakla@evosoft.com>
2020-02-20 17:42:25 +0100

    working

---
+++ proreact16/html_cssprimer/src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export default class App extends Component{
+
+   render = () =>
+       <h4 className="bg-primary text-white text-center p-2 m-1">
+        Component Element
+       </h4>
+
+
+}
