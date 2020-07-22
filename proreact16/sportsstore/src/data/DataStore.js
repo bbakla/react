8b68d06a1ecf34a235a87b71a7c1d9dea7efcb7e
Baris Bakla <baris.bakla@evosoft.com>
2020-07-22 17:01:31 +0200

    unused projects removed

--- proreact16/sportsstore/src/data/DataStore.js
+++
@@ -1,7 +0,0 @@
-import {createStore} from "redux";
-import {ShopReducer} from "./ShopReducer";
-import {CommonReducer} from "./CommonReducer";
-import {CartReducer} from "./CardReducer";
-
-export const SportsStoreDataStore = createStore(CommonReducer(ShopReducer, CartReducer));
-
